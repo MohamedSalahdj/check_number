4de01807e76ded51efbb5e1b177a1a3e0401d279
MohamedSalahdj <mohamedsamydj@gmail.com>
2023-03-11 14:20:59 +0200

    editd in python file in print statment

--- test-number.py
+++ test-number.py
@@ -3,4 +3,4 @@ number = int(input("Enter Your number: "))
 if number % 2 == 0:
     print(f"This number '{number}' is Even")
 else:
-    print(f"This number '{number}'3 is Odd")
+    print(f"This number '{number}' is Odd")
